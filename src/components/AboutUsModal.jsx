6fa3c243acd45df4b609ef651afc230728647e0a
Naresh Jagadeesan <consultingkael@gmail.com>
2023-01-24 17:48:57 +0000

    Modified favicon icons, centered main container

--- src/components/AboutUsModal.jsx
+++ src/components/AboutUsModal.jsx
@@ -16,7 +16,7 @@ const AboutUsModal = (props) => {
       className="modal-container"
     >
       <h2>Enjoying the game?</h2>
-      <p>Please consider checking out some of my other works on <a href="https://github.com/Infinage/tenzies-react">Github</a>.</p>
+      <p>Please consider checking out some of my other works on <a href="https://github.com/Infinage/">Github</a>.</p>
       <div className='modal-container-btns'>
         {props.isPWASupported && <button onClick={handleInstall}>Install Tenzies</button> }
         <button onClick={() => props.setIsOpen(false)}>Close</button>
